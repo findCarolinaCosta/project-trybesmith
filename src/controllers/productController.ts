267a451e8fbacf4043847bca0645bf8fe55ec830
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-29 16:13:19 -0300

    Refactor name for used productService

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -2,18 +2,18 @@ import { Request, Response } from 'express';
 import ProductService from '../services/productService';
 
 export default class ProductController {
-  constructor(private postService = new ProductService()) { }
+  constructor(private productService = new ProductService()) { }
 
   public getAll = async (_req: Request, res: Response) => {
-    const products = await this.postService.getAll();
+    const products = await this.productService.getAll();
     return res.status(200).json(products);
   };
 
   public create = async (req: Request, res: Response) => {
     const product = req.body;
 
-    const postCreated = await this.postService.create(product);
+    const productCreated = await this.productService.create(product);
 
-    return res.status(201).json(postCreated);
+    return res.status(201).json(productCreated);
   };
 }
