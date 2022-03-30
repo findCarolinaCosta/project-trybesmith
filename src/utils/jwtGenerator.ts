31a5217379a3e21bd9e452084eba12bb477d1d31
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-30 12:19:03 -0300

    Set secret key option to string temporarily

--- src/utils/jwtGenerator.ts
+++ src/utils/jwtGenerator.ts
@@ -4,7 +4,7 @@ const jwtConfig = {
   expiresIn: '1d',
 };
 
-const SECRET: string | undefined = process.env.SECRET_KEY || '';
+const SECRET: string | undefined = process.env.SECRET_KEY || 'superSecretKey';
 
 const createToken = (data: object = {}) => jwt.sign({ data }, SECRET, jwtConfig);
 
